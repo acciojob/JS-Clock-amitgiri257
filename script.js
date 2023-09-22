3d97ff2d2f159392a0287c3ce5d4862729f7b72c
amitgiri257 <127419759+amitgiri257@users.noreply.github.com>
2023-09-22 20:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+function rotateClockHands() {
+    const hourHand = document.querySelector(".hour-hand");
+    const minuteHand = document.querySelector(".min-hand");
+    const secondHand = document.querySelector(".second-hand");
+
+    const now = new Date();
+    const hours = now.getHours() % 12;
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hourDegree = (360 / 12) * hours + (360 / 12) * (minutes / 60);
+    const minuteDegree = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
+    const secondDegree = (360 / 60) * seconds;
+
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+}
+
+// Update the clock every second
+setInterval(rotateClockHands, 1000);
+
+// Initial call to set the clock hands correctly on page load
+rotateClockHands();
